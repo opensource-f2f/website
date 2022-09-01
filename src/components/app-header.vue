940d610bb3f2e2252acfe15a398b40bf0a12ed44
三咲智子 <sxzz@sxzz.moe>
2022-09-01 21:37:33 +0800

    feat: add link to logo

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -3,7 +3,9 @@
 <template>
   <header class="header" flex="~ row" h-240px w-full justify-center>
     <div flex="~ col gap-16px" justify-center items-center>
-      <img w-100px h-100px src="../assets/logo.png" />
+      <router-link :to="{ name: 'Index' }">
+        <img w-100px h-100px src="../assets/logo.png" />
+      </router-link>
       <span>开源面对面，连接热爱开源的你！</span>
     </div>
   </header>
