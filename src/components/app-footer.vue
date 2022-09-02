a6ff9ad881223ac0c3425f89cb0d97393c4c6f20
三咲智子 <sxzz@sxzz.moe>
2022-09-02 15:06:39 +0800

    refactor: improve footer style

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,12 +1,18 @@
 <template>
-  <footer class="footer" text-center py-6>
+  <footer text-center py-6 leading-7>
     <p text-sm>
       Designed by
       <a href="https://pan.icu/" target="_blank">内核恐慌</a>,
       <a href="https://github.com/kang8" target="_blank">Kang</a>
       and <a href="https://github.com/sxzz" target="_blank">三咲智子</a>
     </p>
-    <p>Made with ❤️</p>
-    <p>开源面对面 &copy; 2022</p>
+    <p>
+      Made by
+      <a href="https://github.com/sxzz" target="_blank">三咲智子</a> with ❤️
+    </p>
+    <p>
+      <a href="https://github.com/opensource-f2f" target="_blank">开源面对面</a>
+      &copy; 2022
+    </p>
   </footer>
 </template>
