2681f7dbe88f18d27c3f7a71913302871db27575
Frozen FIsh <76603360+sudongyuer@users.noreply.github.com>
2022-09-28 11:42:45 +0800

    chore: update footer (#19)

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -14,9 +14,9 @@
       <a href="https://github.com/opensource-f2f" target="_blank">开源面对面</a>
       &copy; 2022
 
-      <a href="https://beian.miit.gov.cn/" target="_blank">
-        京ICP备2022023077号
-      </a>
+      <a href="https://beian.miit.gov.cn/" target="_blank"
+        >京ICP备2022023077号-1</a
+      >
 
       Sponsored by <a href="https://www.agora.io/cn/" target="_blank">声网</a>
     </p>
