cb230815610d6c8f7cf736bd2395815ac8dc4439
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-10-07 14:32:07 +0800

    feat: add some useful badges on the header (#22)

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const nowrap = 'float: left; white-space: nowrap; margin-left: 3px'
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
@@ -17,6 +18,45 @@ const toggleDark = useToggle(isDark)
         <img w-100px h-100px src="../assets/logo.png" />
       </router-link>
       <span>开源面对面，连接热爱开源的你！</span>
+
+      <span>
+        <a
+          target="_blank"
+          href="https://github.com/opensource-f2f"
+          :style="nowrap"
+        >
+          <img
+            alt="GitHub Org's stars"
+            src="https://img.shields.io/github/stars/opensource-f2f?style=social"
+          />
+        </a>
+        <a target="_blank" href="https://twitter.com/osf2f" :style="nowrap">
+          <img
+            alt="Twitter followers"
+            src="https://img.shields.io/twitter/follow/osf2f?style=social"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.youtube.com/channel/UCV7Ibg1k_aMSEcDFgJvuvdg"
+          :style="nowrap"
+        >
+          <img
+            alt="YouTube subscribers"
+            src="https://img.shields.io/youtube/channel/subscribers/UCV7Ibg1k_aMSEcDFgJvuvdg?style=social"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.ximalaya.com/album/53320813.xml"
+          :style="nowrap"
+        >
+          <img
+            alt="RSS address"
+            src="https://img.shields.io/badge/rss-F88900?style=flat&logo=rss&logoColor=white"
+          />
+        </a>
+      </span>
     </div>
     <div
       absolute
