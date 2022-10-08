3ec09396d1b4c5eaed74a4d10a9ec26eaecffd46
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-10-08 09:47:30 +0800

    feat: improve the episode metadata parse (#24)
    Co-authored-by: 三咲智子 <sxzz@sxzz.moe>

--- src/utils/episode.ts
+++ src/utils/episode.ts
@@ -3,6 +3,7 @@ import type { Component } from 'vue'
 export type Person = string | { name: string; link: string }
 
 export interface RawEpisode {
+  num: string
   date: string
   title: string
   hosts?: Person[]
@@ -16,19 +17,14 @@ export interface Episode extends Omit<RawEpisode, 'date'> {
   date: Date
 }
 
-export const getEpisodeId = (filename: string) => {
-  return /episodes[/\\](.*?)\.md$/.exec(filename)?.[1] ?? undefined
-}
-
 export const getAllEpisode = () =>
   Object.entries(
     import.meta.glob<RawEpisode>('../episodes/*.md', { eager: true })
-  ).map(([filename, module]): Episode => {
-    const id = getEpisodeId(filename)!
+  ).map(([, module]): Episode => {
     return {
       ...module,
+      id: module.num,
       date: new Date(module.date),
-      id,
     }
   })
 
