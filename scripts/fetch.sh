868620da3341b0bc64a10de7d5b1160f38267da1
三咲智子 <sxzz@sxzz.moe>
2022-10-08 09:53:14 +0800

    refactor: use degit

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,7 +1,7 @@
 # clean
 rm -rf content src/episodes
 
-git clone https://github.com/opensource-f2f/episode content
+pnpx degit https://github.com/opensource-f2f/episode content
 
 mkdir -p src/episodes
 cp content/shownotes/* src/episodes
