5174614ee4f5a3a05fa678c0f1261af42df7fddf
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-10-12 15:20:09 +0800

    feat: add audio link (#27)
    Co-authored-by: 三咲智子 <sxzz@sxzz.moe>

--- src/components/episode-content.vue
+++ src/components/episode-content.vue
@@ -12,6 +12,12 @@ const episode = $computed(() => episodes.find((episode) => episode.id === id))
   <div v-if="episode" container mx-auto px-4 mt-5>
     <div mb-5>
       <h1 text-center text-2xl font-bold mb-2 v-html="episode.title" />
+      <div flex justify-center my-2>
+        <a :href="episode.link" target="_blank">
+          <img title="喜马拉雅" src="../assets/xmly.png" w-20px />
+        </a>
+      </div>
+
       <episode-meta :info="episode" justify-center />
     </div>
 
