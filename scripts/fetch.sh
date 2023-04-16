65b2202a2919a19e47d3267cab9172ea6cd67c16
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-04-17 02:25:04 +0800

    chore: update fetch script

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 # clean
-# See also https://www.npmjs.com/package/degit
 rm -rf content src/episodes
 
+# See also https://www.npmjs.com/package/degit
 pnpx degit https://github.com/opensource-f2f/episode content
 
 mkdir -p src/episodes
@@ -11,3 +13,5 @@ rm -rf src/episodes/README.md
 mkdir -p src/contributors
 cp content/archives/* src/contributors
 rm -rf src/contributors/README.md
+
+rm -rf content
