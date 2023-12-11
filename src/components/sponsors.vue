cd0b000a35cace82091e1ab437450735206e7b8c
rick <LinuxSuRen@users.noreply.github.com>
2023-12-11 03:16:39 +0000

    feat: add the sponsors page

---
+++ src/components/sponsors.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import sponsors from '../assets/sponsors.json'
+
+sponsors.sort((a, b) => {
+  return Date.parse(b.date) - Date.parse(a.date)
+})
+</script>
+
+<template>
+  <div style="align-self: center">
+    <div>Sponsor list</div>
+
+    <table style="width: 600px">
+      <tr>
+        <td>捐赠人</td>
+        <td>时间</td>
+        <td>捐赠物</td>
+        <td>渠道</td>
+      </tr>
+      <tr v-for="sponsor in sponsors" :key="sponsor.name">
+        <td>{{ sponsor.name }}</td>
+        <td>{{ sponsor.date }}</td>
+        <td>{{ sponsor.item }}</td>
+        <td>{{ sponsor.source }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
