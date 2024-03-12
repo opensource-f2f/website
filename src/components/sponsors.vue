463b41ee49119b00ed8c9af0303a8168705aaa06
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-03-12 15:14:16 +0800

    chore: adjust table width

--- src/components/sponsors.vue
+++ src/components/sponsors.vue
@@ -9,7 +9,7 @@ sponsors.sort((a, b) => {
 <template>
   <div flex="~ col gap2" items-center p2>
     <h1>Sponsor list</h1>
-    <table max-w-500px w-full>
+    <table max-w-full w-600px>
       <tr>
         <td>捐赠人</td>
         <td>时间</td>
