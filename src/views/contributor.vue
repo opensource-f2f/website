b5ab7603e6028616fd11f80457ba5fe554d7861e
王佳鑫 <1778368831@qq.com>
2024-03-16 16:28:47 +0800

    refactor: extract contributor

--- src/views/contributor.vue
+++ src/views/contributor.vue
@@ -26,35 +26,57 @@ if (contributor.value)
 </script>
 
 <template>
-  <div v-if="contributor" mx-auto mt-5 px-4 container>
-    <div mb-5>
-      <h1 mb-2 text-center text-2xl font-bold v-html="contributor.name" />
+  <div v-if="contributor" mx10>
+    <div flex="~ col items-center">
+      <!-- avatars -->
+      <img
+        :src="`https://avatars.githubusercontent.com/${contributor.github}`"
+        :alt="contributor.name"
+        my5
+        w-100px
+        rd-3
+      />
 
-      <a :href="`https://github.com/${contributor.github}`" target="_blank">
-        <img
-          :src="`https://avatars.githubusercontent.com/${contributor.github}`"
-          :alt="contributor.github"
-          class="w-128px"
-        />
-      </a>
+      <!-- name -->
+      <h1 text-2xl font-bold v-html="contributor.name" />
 
-      <span v-if="contributor.twitter">
-        Twitter:
-        <a :href="`https://twitter.com/${contributor.twitter}`" target="_blank">
-          {{ contributor.twitter }}
-        </a>
-      </span>
+      <!-- title -->
+      <span v-if="contributor.title">{{ contributor.title }}</span>
 
-      <component :is="contributor.default" />
+      <!-- social -->
+      <div>
+        <span v-if="contributor.github">
+          Github:
+          <a :href="`https://github.com/${contributor.github}`" target="_blank">
+            {{ contributor.github }}
+          </a>
+        </span>
+        <span v-if="contributor.twitter">
+          Twitter:
+          <a
+            :href="`https://twitter.com/${contributor.twitter}`"
+            target="_blank"
+          >
+            {{ contributor.twitter }}
+          </a>
+        </span>
+      </div>
 
-      <span v-if="contributor.audios">
+      <!-- profile -->
+      <div mt5 pb15 pt5 container border-y="1px solid [var(--border-color)]">
+        <component :is="contributor.default" />
+      </div>
+
+      <!-- audios -->
+      <span v-if="contributor.audios" mt5>
         Podcast List:
         <li v-for="audio in contributor.audios" :key="audio.title">
           <a :href="`${audio.link}`" target="_blank">{{ audio.title }}</a>
         </li>
       </span>
 
-      <span v-if="contributor.videos">
+      <!-- videos -->
+      <span v-if="contributor.videos" mt5>
         Video List:
         <li v-for="video in contributor.videos" :key="video.title">
           <a :href="`${video.link}`" target="_blank">{{ video.title }}</a>
